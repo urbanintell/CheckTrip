43328d7e275f22a743281c35d3c3dd4d6dfc54d9
hwu353 <haowu1112@gmail.com>
2016-10-30 16:47:02 -0400

    tsaInfo

---
+++ CheckTrip/TSAInfo.swift
@@ -0,0 +1,17 @@
+//
+//  TSAInfo.swift
+//  CheckTrip
+//
+//  Created by Hao Wu on 10/29/16.
+//  Copyright © 2016 Lusenii Kromah. All rights reserved.
+//
+
+import Foundation
+
+class TSAInfo{
+    
+    var WaitTimeIndex:String = ""
+    var Created_Datetime:String = ""
+    var CheckpointIndex:String = ""
+ 
+}
